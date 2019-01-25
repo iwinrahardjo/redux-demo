cda31470288d1c41da1b73e4a9e09186a8e91743
Iwin Rahardjo <iwinrahardjo@yahoo.com>
2019-01-25 14:54:35 +0700

    make a simple redux file with react

---
+++ src/components/Counter.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <button> + </button>
+      <button> - </button>
+    </div>
+  );
+};
+
+export default Counter;

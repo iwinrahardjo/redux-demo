2ba419f283d2534251422641ed8d514fb4bfcfd6
Iwin Rahardjo <iwinrahardjo@yahoo.com>
2019-01-25 15:19:23 +0700

    connecting redux to react

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,12 +1,12 @@
 import { INCREMENT, DECREMENT } from "../types";
 
-export default (state=0, action) => {
-  switch {action.type} {
-    case INCREMENT: 
-      return state + 1 
+export default (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
     case DECREMENT:
-      return state - 1
+      return state - 1;
     default:
-      return state
+      return state;
   }
 };

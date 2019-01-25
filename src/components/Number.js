a6f07100820f20d90c8f3d244869b1a5d9955cbc
Iwin Rahardjo <iwinrahardjo@yahoo.com>
2019-01-25 15:38:36 +0700

    refactor and edit reducers's conter

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const Number = () => {
   return <h1>0</h1>;
 };
 
-export default Number;
+const mapStateToProps = store => ({
+  number: store.counter.number
+});
+
+export default connect()(Number);

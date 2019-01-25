df4990bc80fb5a103cd44fdb5fe42d2e66e470e3
Iwin Rahardjo <iwinrahardjo@yahoo.com>
2019-01-25 15:02:15 +0700

    adding function at reducers/counter.js

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,3 +1,12 @@
 import { INCREMENT, DECREMENT } from "../types";
 
-export default (state, action) => {};
+export default (state=0, action) => {
+  switch {action.type} {
+    case INCREMENT: 
+      return state + 1 
+    case DECREMENT:
+      return state - 1
+    default:
+      return state
+  }
+};

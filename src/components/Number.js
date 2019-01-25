c3de37f8fe23b4f47b860a8d8fe692f7932eff3f
Iwin Rahardjo <iwinrahardjo@yahoo.com>
2019-01-25 15:51:29 +0700

    add configuration to component's number

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const Number = () => {
-  return <h1>0</h1>;
-};
-
 const mapStateToProps = store => ({
   number: store.counter.number
 });
 
-export default connect()(Number);
+const Number = props => {
+  return <h1>{props.number}</h1>;
+};
+
+// const Number = props => {
+//   const { number } = props;
+// return <h1>{number}</h1>;
+// };
+
+// const Number = ({ number }) => {
+//   return <h1>{number}</h1>;
+// };
+
+export default connect(mapStateToProps)(Number);
